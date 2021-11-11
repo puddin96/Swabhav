c78052f94d404a83c67cf2d4f34f4a2f8bc8395c
Friday <sutarshivam270@gmail.com>
2021-11-11 19:35:07 +0530

    done for the day

--- SrpSolution/SrpSolutionApp/Program.cs
+++ SrpSolution/SrpSolutionApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SrpSolutionApp.Model;
 
 namespace SrpSolutionApp
 {
@@ -10,6 +11,9 @@ namespace SrpSolutionApp
     {
         static void Main(string[] args)
         {
+            Invoice invoice = new Invoice(101, "One Plus 6T", 45000, 10f, 25f);
+            InvoiceHtmlGenerator htmlGenerator = new InvoiceHtmlGenerator();
+            htmlGenerator.SaveInvoice(invoice);
         }
     }
 }
